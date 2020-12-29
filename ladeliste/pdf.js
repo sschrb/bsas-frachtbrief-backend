d1a3f77c42cf990a8af5fde69815537f154c3680
schreiber <soerenschrb@gmail.com>
2020-12-29 19:52:59 +0100

    berechnung summen

--- ladeliste/pdf.js
+++ ladeliste/pdf.js
@@ -61,10 +61,23 @@ docDefinition1.content.push({
     ]
   }
 }, ' ', ' ')
+
+var gesamtsumme = {
+  liter: 0,
+  masse: 0,
+  tara: 0,
+  bruttogew: 0,
+}
+
 for(ladegut in json.ladelistedata){
   //console.log(json.ladelistedata[ladegut].ladegut)
   if(json.ladelistedata[ladegut].ladegut && json.ladelistedata[ladegut].ladegut.nhm){
-
+var zwischensumme = {
+  liter: 0,
+  masse: 0,
+  tara: 0,
+  bruttogew: 0,
+}
     var basetable = {
       style: 'tableExample',
       table: {
@@ -80,24 +93,33 @@ for(ladegut in json.ladelistedata){
     }
     for(wagen in json.ladelistedata[ladegut].wagen){
       console.log(json.ladelistedata[ladegut].wagen[wagen])
+
+      zwischensumme.masse= (json.ladelistedata[ladegut].wagen[wagen].liter*json.ladelistedata[ladegut].ladegut.dichte) + zwischensumme.masse;
+      zwischensumme.liter= Number(json.ladelistedata[ladegut].wagen[wagen].liter) + zwischensumme.liter
+      zwischensumme.tara=json.ladelistedata[ladegut].wagen[wagen].wagendaten.eigengewicht + zwischensumme.tara
+      zwischensumme.bruttogew=(json.ladelistedata[ladegut].wagen[wagen].liter*json.ladelistedata[ladegut].ladegut.dichte) + json.ladelistedata[ladegut].wagen[wagen].wagendaten.eigengewicht + zwischensumme.bruttogew
+
       basetable.table.body.push( [{text: i, fontSize: 8}, {text: json.ladelistedata[ladegut].wagen[wagen].wagendaten.wagennummer, fontSize: 8},
          {text: json.ladelistedata[ladegut].wagen[wagen].wagendaten.achsanzahl, fontSize: 8}, 
          {text: json.ladelistedata[ladegut].ladegut.bezeichnung, fontSize: 8, alignment: 'center'}, 
          {text: json.ladelistedata[ladegut].wagen[wagen].liter, fontSize: 8}, 
          {text: json.ladelistedata[ladegut].ladegut.dichte, fontSize: 8}, 
          {text: json.ladelistedata[ladegut].ladegut.rid, fontSize: 8}, 
-         {text: 'Masse\nLadung (kg)', fontSize: 8}, 
-         {text: 'Tara Wagen\n(kg)', fontSize: 8}, 
-         {text:'Brutto Gew.\nWagen (kg)', fontSize: 8}],
+         {text: (json.ladelistedata[ladegut].wagen[wagen].liter*json.ladelistedata[ladegut].ladegut.dichte).toFixed(2), fontSize: 8}, 
+         {text: json.ladelistedata[ladegut].wagen[wagen].wagendaten.eigengewicht, fontSize: 8}, 
+         {text: ((json.ladelistedata[ladegut].wagen[wagen].liter*json.ladelistedata[ladegut].ladegut.dichte) + json.ladelistedata[ladegut].wagen[wagen].wagendaten.eigengewicht).toFixed(2), fontSize: 8}],
 )
       i++;
     }
 
 
 
-    basetable.table.body.push([{text: 'Zwischensumme:', colSpan: 4, fontSize: 8, alignment: 'right' },{},{},{}, {text: 'Summe', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'Summe', fontSize: 8}, {text: 'Summe', fontSize: 8}, {text:'Summe', fontSize: 8}],
+    basetable.table.body.push([{text: 'Zwischensumme:', colSpan: 4, fontSize: 8, alignment: 'right' },{},{},{}, {text: zwischensumme.liter.toFixed(2), fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: zwischensumme.masse.toFixed(2), fontSize: 8}, {text: zwischensumme.tara.toFixed(2), fontSize: 8}, {text: zwischensumme.bruttogew.toFixed(2), fontSize: 8}],
     )
-
+    gesamtsumme.masse = zwischensumme.masse + gesamtsumme.masse
+    gesamtsumme.liter = zwischensumme.liter + gesamtsumme.liter
+    gesamtsumme.tara = zwischensumme.tara + gesamtsumme.tara
+    gesamtsumme.bruttogew = zwischensumme.bruttogew + gesamtsumme.bruttogew
 
 
 
@@ -119,6 +141,11 @@ for(ladegut in json.ladelistedata){
   )
 
   }
+
+
+
+
+
 }
 
 docDefinition1.content.push(
@@ -133,7 +160,7 @@ docDefinition1.content.push(
           keepWithHeaderRows: 1,
           widths: [259, 'auto', 'auto', 'auto','auto', 42, 'auto'],
           body: [
-              [{text: 'Gesamtsumme:', fontSize: 8, alignment: 'right' }, {text: 'Summe\n(I Ladung)', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'Summe\n(kg Ladung)', fontSize: 8}, {text: 'Summe\n(kg Tara)', fontSize: 8}, {text:'Summe\n(kg Brutto)', fontSize: 8}],
+              [{text: 'Gesamtsumme:', fontSize: 8, alignment: 'right' }, {text: gesamtsumme.liter.toFixed(2)+'\n(I Ladung)', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: gesamtsumme.masse.toFixed(2)+'\n(kg Ladung)', fontSize: 8}, {text: gesamtsumme.tara+'\n(kg Tara)', fontSize: 8}, {text:gesamtsumme.bruttogew.toFixed(2)+'\n(kg Brutto)', fontSize: 8}],
   
                     ]
         }
@@ -142,62 +169,6 @@ docDefinition1.content.push(
 )
 
 
-
-  var docDefinition = {
-    content: [
-      
-      {text:'Ladeliste', fontSize: 14},
-      ' ',
-      {
-        style: 'tableExample',
-        table: {
-          widths: ['*', '*', '*'],
-          body: [
-            ['Absender\n Name Absender\n Land-PLZ Ort', 'Empfänger\n [Name Empfänger]\n [Land-PLZ Ort]', 'Datum\n [Abweichendes Feld zu 29]'],
-            ['Abgangsbahnhof\n [Abgangsbahnhof]-[Land]', 'Zielbahnhof\n [Zielbahnhof Name]-[Land]', 'Referenz\n [siehe Feld 8]']
-          ]
-        }
-      },
-      ' ',
-      {
-        style: 'tableExample',
-        table: {
-          headerRows: 1,
-          // dontBreakRows: true,
-          keepWithHeaderRows: 1,
-          widths: ['auto', 'auto', 'auto', 145,'auto', 'auto', 'auto','auto', 'auto', 'auto'],
-          body: [
-            [{text: 'Nr.', fontSize: 8}, {text: 'Wagennummer', fontSize: 8}, {text: 'Achs-\n anzahl', fontSize: 8}, {text: 'Bezeichnung des Gutes', fontSize: 8, alignment: 'center'}, {text: 'Liter', fontSize: 8}, {text: 'Dichte', fontSize: 8}, {text: 'RID', fontSize: 8}, {text: 'Masse\nLadung (kg)', fontSize: 8}, {text: 'Tara Wagen\n(kg)', fontSize: 8}, {text:'Brutto Gew.\nWagen (kg)', fontSize: 8}],
-            [{text: 'Nr.', fontSize: 8}, {text: 'Wagennummer', fontSize: 8}, {text: 'Achs-\n anzahl', fontSize: 8}, {text: 'Bezeichnung des Gutes', fontSize: 8, alignment: 'center'}, {text: 'Liter', fontSize: 8}, {text: 'Dichte', fontSize: 8}, {text: 'RID', fontSize: 8}, {text: 'Masse\nLadung (kg)', fontSize: 8}, {text: 'Tara Wagen\n(kg)', fontSize: 8}, {text:'Brutto Gew.\nWagen (kg)', fontSize: 8}],
-            [{text: 'Zwischensumme:', colSpan: 4, fontSize: 8, alignment: 'right' },{},{},{}, {text: 'Summe', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'Summe', fontSize: 8}, {text: 'Summe', fontSize: 8}, {text:'Summe', fontSize: 8}],
-  
-                    ]
-        }
-      },
-      ' ',
-      
-      {text:'Bemerkung:\n Bemerkung Ladegut 1', fontSize: 8},
-      ' ',
-      {text:'NHM-Code: Code Ladegut1', fontSize: 8},
-      
-      ' ',
-      ' ',
-      
-      {
-        style: 'tableExample',
-        table: {
-          headerRows: 1,
-          // dontBreakRows: true,
-          keepWithHeaderRows: 1,
-          widths: [259, 'auto', 'auto', 'auto','auto', 42, 'auto'],
-          body: [
-              [{text: 'Gesamtsumme:', fontSize: 8, alignment: 'right' }, {text: 'Summe\n(I Ladung)', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'n.n.', fontSize: 8}, {text: 'Summe\n(kg Ladung)', fontSize: 8}, {text: 'Summe\n(kg Tara)', fontSize: 8}, {text:'Summe\n(kg Brutto)', fontSize: 8}],
-  
-                    ]
-        }
-      },
-    ]
-  };
   
   var pdfDoc = printer.createPdfKitDocument(docDefinition1)
     
