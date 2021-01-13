c911362e01cb153dc5ed4dbc3061d94fb8dd63e9
schreiber <soerenschrb@gmail.com>
2021-01-13 17:17:49 +0100

    postgres unterstützung

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use('/ladeliste', require('./ladeliste/ladeliste.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = 4000;
 app.listen(port, () => console.log('Server listening on port ' + port));
